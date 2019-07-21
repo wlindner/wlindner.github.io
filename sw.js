dbbedda579725657adbbc6b233c57264635647f0
William Lindner <wlindner522@gmail.com>
2019-07-20 17:45:59 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a136afa3cc3cbf7db757.js"
+    "url": "webpack-runtime-3255521ec9f21161c232.js"
   },
   {
     "url": "app-e8293bb446ca207eba86.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "46d386e7cd4555233a7354a7ebc3eba4"
+    "revision": "56d55f5a13515a34350326fe3ce4851b"
   },
   {
     "url": "0-8ae77b9460d07cd1aa24.js"
